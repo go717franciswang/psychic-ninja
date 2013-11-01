cb873164e08814d503cf31757958ce0ee3f8b792
Francis Wang <go717franciswang@hotmail.com>
2013-10-31 23:29:37 -0700

    Past page 69: multi-thread

--- src/com/scoobydoo/yamba/StatusActivity.java
+++ src/com/scoobydoo/yamba/StatusActivity.java
@@ -1,7 +1,9 @@
 package com.scoobydoo.yamba;
 
 import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -9,6 +11,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class StatusActivity extends Activity implements OnClickListener{
 	private static final String TAG = "StatusActivity";
@@ -30,8 +33,30 @@ public class StatusActivity extends Activity implements OnClickListener{
         twitter.setAPIRootUrl("http://yamba.marakana.com/api");
     }
     
+    class PostToTwitter extends AsyncTask<String, Integer, String> {
+
+		@Override
+		protected String doInBackground(String... statuses) {
+			try {
+				Twitter.Status status = twitter.updateStatus(statuses[0]);
+				return status.text;
+			} catch (TwitterException e) {
+				Log.e(TAG, e.toString());
+				e.printStackTrace();
+				return "Failed to post";
+			}
+		}
+		
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+		}
+    	
+    }
+    
     public void onClick(View v) {
-    	twitter.setStatus(editText.getText().toString());
+    	String status = editText.getText().toString();
+    	new PostToTwitter().execute(status);
     	Log.d(TAG, "onClicked");
     }
 
